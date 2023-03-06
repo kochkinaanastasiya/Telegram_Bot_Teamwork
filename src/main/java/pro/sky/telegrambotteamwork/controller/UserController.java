877a4f758215454f1acbc5f6c1ee80f7e34910fe
Anastasiya Kochkina <kochkinaanastasiya@yandex.ru>
2023-03-06 23:38:21 +0300

    Описание API в UserController

--- src/main/java/pro/sky/telegrambotteamwork/controller/UserController.java
+++ src/main/java/pro/sky/telegrambotteamwork/controller/UserController.java
@@ -1,6 +1,11 @@
 package pro.sky.telegrambotteamwork.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.telegrambotteamwork.model.User;
 import pro.sky.telegrambotteamwork.service.UserService;
@@ -16,16 +21,46 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Информация о пользователе",
+            description = "Позволяет получить информацию о пользователе из системы")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = User.class))
+    )
+
     @GetMapping("{id}")
     public User getUserInfo(@PathVariable Long id) {
         return userService.readInfo(id);
     }
+
+    @Operation(
+            summary = "Регистрация нового пользователя",
+            description = "Позволяет зарегистрировать нового пользователя в системе")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = User.class))
+    )
 
     @PostMapping
     public HttpStatus registerNewUser(@RequestBody User user) {
         userService.addUser(user);
         return HttpStatus.OK;
     }
+
+    @Operation(
+            summary = "Удаление пользователя",
+            description = "Позволяет удалить пользователя из системы")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation =  User.class))
+    )
 
     @DeleteMapping
     public HttpStatus deleteUser(@RequestBody User user) {
